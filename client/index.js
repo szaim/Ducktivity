93b9ec3bc54b429bb8f4b3f383d967978117b4b1
szaim <zaimsofia81@gmail.com>
2016-09-08 14:17:42 -0400

    connecting server with the client

--- client/index.js
+++ client/index.js
@@ -2,15 +2,23 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Provider = require("react-redux").Provider;
 var store = require("./store");
+var actions = require("./action");
 var connect = require("react-redux").connect;
 
 var HelloWorld = React.createClass({
-	render: function() {
-		return <div>{this.props.message}</div>
+	componentWillMount: function(){
+		console.log("before dispatch", this.props.message);
+		this.props.dispatch(actions.fetchData());
+	},
+	render: function(){
+		console.log("after dispatch ", this.props.message);
+		return (
+			<div>{this.props.message}</div>)
 	}
-})
+});
 
 var mapStateToProps = function(state, props) {
+
     return {
         message: state.message
     };
@@ -25,7 +33,25 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+// var Board = React.createClass({
+// 	componentWillMount: function(){
+// 		console.log("before dispatch", this.props.message);
+// 		this.props.dispatch(actions.fetchData());
+// 	},
+// 	render: function(){
+// 		console.log("after dispatch ", this.props.message);
+// 		return(
+// 			<div className='Board'>
+// 				{this.props.message}
+// 				<h1 className='text-center'>InstaWinner</h1>
+// 				<SearchBar />
+// 				<ImageList />
+				
 
+// 			</div>
+// 		);
+// 	}
+// });
 
 
 
--- client/reducer.js
+++ client/reducer.js
@@ -1,16 +1,24 @@
 
-var initalState = {
-	message: "Sofy"
-}
+var actions = require("./action");
 
+var initialState = {};
 
-
-var reducerCreator = function (state) {
-	// var state = state || initalState;
-	return {
-		message: "Sofy"
+var reducerCreator = function (state, action) {
+	state = state || initialState;
+	if (action.type === actions.FETCH_DATA_SUCCESS) {
+		return {
+			message: action.data.message
 		}
+	}
+	if (action.type === actions.FETCH_DATA_ERROR) {
+		return {
+			error: action.error
+		}
+	}
+	return state;
 }
 
 // exports.reducerCreator = reducerCreator;
+
+
 module.exports = reducerCreator;
