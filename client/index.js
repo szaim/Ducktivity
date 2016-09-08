52933adc6048214e0ff40fcfa25123464af8bd05
szaim <zaimsofia81@gmail.com>
2016-09-08 12:22:04 -0400

    connecting redux with react

--- client/index.js
+++ client/index.js
@@ -1,12 +1,31 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Provider = require("react-redux").Provider;
+var store = require("./store");
+var connect = require("react-redux").connect;
 
-var HelloWorld = function() {
-    return (
-        <div>Hello World!</div>
-    );
+var HelloWorld = React.createClass({
+	render: function() {
+		return <div>{this.props.message}</div>
+	}
+})
+
+var mapStateToProps = function(state, props) {
+    return {
+        message: state.message
+    };
 };
 
+var Container = connect(mapStateToProps)(HelloWorld);
+
 document.addEventListener('DOMContentLoaded', function() {
-    ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+    ReactDOM.render( <Provider store={store}>
+            <Container />
+        </Provider>, document.getElementById('app'));
 });
+
+
+
+
+
+
---
+++ client/reducer.js
@@ -0,0 +1,16 @@
+
+var initalState = {
+	message: "Sofy"
+}
+
+
+
+var reducerCreator = function (state) {
+	// var state = state || initalState;
+	return {
+		message: "Sofy"
+		}
+}
+
+// exports.reducerCreator = reducerCreator;
+module.exports = reducerCreator;
