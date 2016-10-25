039e10b3b89897061be564b6e907c8b46fb7c818
szaim <zaimsofia81@gmail.com>
2016-10-24 23:48:29 -0400

    first commit

--- client/index.js
+++
@@ -1,8 +0,0 @@
-var React = require("react");
-import { render } from 'react-dom';
-require("../style.css");
-var routes = require ('./components/routes');
-// import Main from './components/app';
-
-
-render(routes, document.getElementById('app'));
--- server/index.js
+++ server/index.js
@@ -1,25 +1,11 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
-var mongoose = require("mongoose");
-var Megaman = require("./schema")
+
 app.use(bodyParser.json());
 
 app.use('/', express.static('build'));
 
-var db = 'mongodb://localhost:27017/api';
-mongoose.connect(db);
-
-app.get("/app", function(req, res) {
-	Megaman.find({})
-		   .exec(function(err, Megaman) {
-				if (err) {
-					console.log("Error has occured");
-				} else {
-					res.json(Megaman[0].list);
-				}
-			});
-});
 
 
 app.listen(8080, function () {
