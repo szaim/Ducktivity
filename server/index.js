4ae9b293a1a8165a57315c1667e7c7e0773631e8
yolixtly <yolixtly_13@hotmail.com>
2016-10-25 14:50:26 -0400

    Get endpoint working

--- server/index.js
+++ server/index.js
@@ -1,16 +1,46 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
+var Task = require('./models/task');
+var mongoose = require('mongoose');
+var config = require('./config');
 
 app.use(bodyParser.json());
 
 app.use('/', express.static('build'));
 
-app.get("/app", function(req, res) {
-	console.log("hitting endpoint");
-	res.json({message: "hello world"})
+/*Connection to MongoDB/mongoose */
+var runServer = function(callback) {
+    mongoose.connect(config.DATABASE_URL, function(err) {
+        if (err && callback) {
+            return callback(err);
+        }
+        
+        app.listen(config.PORT, function() {
+            console.log('Listening on localhost:' + config.PORT);
+            if (callback) {
+                callback();
+            }
+        });
+    });
+};
+if (require.main === module) {
+    runServer(function(err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+};
+
+app.get('/api', function(req, res) {
+	Task.find({})
+		.exec(function(err, tasks){
+			if (err) {
+            console.log('error', err);
+        	} else {
+        		res.json(tasks);
+        	}
+		});
 });
 
-app.listen(8080, function () {
-  console.log('Listening at 8080!');
-});
+
