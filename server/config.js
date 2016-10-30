223337a44c33618ef84f6fb95d81dba845d744da
yolixtly <yolixtly_13@hotmail.com>
2016-10-30 19:00:39 -0400

    connected to mLab

--- server/config.js
+++ server/config.js
@@ -1,6 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
     global.DATABASE_URL ||
-    (process.env.NODE_ENV === 'production' ?
-        'mongodb://localhost/data/' :
-        'mongodb://localhost/data/dev');
+    (process.env.NODE_ENV = 'mongodb://ducktivity:ducktivity123@ds139267.mlab.com:39267/ducktivity');
 exports.PORT = 8080 || process.env.PORT;
