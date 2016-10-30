b91a29dea0de4248d159b6e4ef14798953ed009b
yolixtly <yolixtly_13@hotmail.com>
2016-10-30 19:44:17 -0400

    modified port

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
     global.DATABASE_URL ||
     (process.env.NODE_ENV = 'mongodb://ducktivity:ducktivity123@ds139267.mlab.com:39267/ducktivity');
-exports.PORT = 8080 || process.env.PORT;
+exports.PORT = process.env.PORT || 8080;
