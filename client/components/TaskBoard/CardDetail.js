43d517c926d1a60f159aa474022ae2330b5906c6
mrmojica <mrmojica510@gmail.com>
2016-11-01 17:46:26 -0700

    created card edit function

--- client/components/TaskBoard/CardDetail.js
+++ client/components/TaskBoard/CardDetail.js
@@ -1,13 +1,44 @@
-var React = require('react')
+var React = require('react');
+var ReactDOM = require('react-dom');
+var connect = require('react-redux').connect;
 
-var CardDetail = function(props) {
-  return(
-    <div className="task-item">
-     <h3>{props.title}</h3>
-      <button className='delete-task-button' type='submit' onClick={props.handleCardDelete}>Delete Task</button>
-    </div>
+var CardDetail = React.createClass({
+	  getInitialState: function() {
+	  	return {
+	  		contentEditable: false,
+	  		buttonAction: 'Edit'
+	  	};
+	  },
 
-  );
-}
+	  editable: function() {
+	  	if(this.state.buttonAction == 'Save') {
+	  		this.setState({
+	  			contentEditable: false,
+	  			buttonAction: 'Edit'
+	  		})
+	  	}else{
+	  	this.setState({
+	  		contentEditable: true,
+	  		buttonAction: 'Save'
+	  	});
+	  }
+	},
+
+
+
+
+
+
+	  render: function() {
+
+	  return(
+	    <div className="task-item">
+	     <h3 contentEditable={this.state.contentEditable}>{this.props.title}</h3><button type='submit' onClick={this.editable}>{this.state.buttonAction}</button>
+	      <button className='delete-task-button' type='submit' onClick={this.props.handleCardDelete}>Delete Task</button>
+	    </div>
+
+	  );
+	}
+});
 
 module.exports= CardDetail;
