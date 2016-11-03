0dcfde4017b868a3956b85b4a87e933cbf2c94db
yolixtly <yolixtly_13@hotmail.com>
2016-11-03 19:14:15 -0400

    delete cards on Overview panel working!

--- client/redux/reducers/overviewReducer.js
+++ client/redux/reducers/overviewReducer.js
@@ -35,8 +35,31 @@ var overviewReducer = function(state, action) {
 
     } else if (action.type === cardConstants.POST_CARD_ERROR) {
         return action.error;
-    } 
+    }  else if (action.type === cardConstants.UPDATE_CARD_SUCCESS) {
+         var new1Objectives = state.objectives.map(function(objective, index) {
+            console.log("action data in UPDATE CARD overviewReducer", action.data);
+            if ('deleted' == action.data.status) {
+                for (var i = 0; i < objective.cards.length; i++) {
+                    if (objective.cards[i]._id == action.data._id) {
+                        console.log('objective', objective);
+                        console.log('objective cards', objective.cards);
+                        objective.cards.splice(i, 1);
+                    }
+                }
+            }
+            return objective;
+        });
+        // console.log('categories!!', categories);
 
+        state = Object.assign({}, state, {
+            objectives: new1Objectives
+        });
+
+        return state;
+
+    } else if (action.type === cardConstants.UPDATE_CARD_ERROR) {
+        return action.error
+    }
     return state;
  };
 
