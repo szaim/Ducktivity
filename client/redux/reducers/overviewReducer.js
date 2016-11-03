c1094c9cb0b336a1a7ee8a41121b62bf3ec9c073
yolixtly <yolixtly_13@hotmail.com>
2016-11-03 17:57:32 -0400

    in progress: add cards to objective panel

--- client/redux/reducers/overviewReducer.js
+++ client/redux/reducers/overviewReducer.js
@@ -1,9 +1,10 @@
 var Constants = require('../constants/overviewConstants');
+var cardConstants = require('../constants/CardCategoriesConstants');
 var update = require('react-addons-update');
 
 var initialState = {
     projectTitle: "",
-    objectives: []
+    objectives: [],
 };
 
 var overviewReducer = function(state, action) {
@@ -11,6 +12,7 @@ var overviewReducer = function(state, action) {
 
     if (action.type === Constants.FETCH_PROJECT_SUCCESS) {
         console.log("fetch actionData project in Reducer", action.data);
+        console.log('overview reducer state', state);
       
       var newState = Object.assign({}, state, {
                     projectTitle: action.data.title,
@@ -20,6 +22,34 @@ var overviewReducer = function(state, action) {
     } else if (action.type === Constants.FETCH_PROJECT_ERROR) {
         return action.error;
     }
+
+        // } else if (action.type === cardConstants.POST_CARD_SUCCESS) {
+    // 	// var index = '';
+    //         var objective = state.objectives.map(function(objective, index) {
+    //         if (objective._id !== action.data.objective) {
+    //             return objective;
+    //         } else {
+    //         	// index = i;
+    //          //    return Object.assign({}, objective, {
+    //          //        cards: objective.cards.concat(action.data)
+    //          // //    });
+    //         	// newState = update(state.objectives, {
+    //         	// 	index: {$push: action.data}
+    //         	// });
+
+    //         }
+
+    //     });
+    //     var objectives = objectives[index];
+    //     state = Object.assign({}, state, {
+    //         objectives: objectives
+    //     });
+    //     return state;
+
+    // } else if (action.type === cardConstants.POST_CARD_ERROR) {
+    //     return action.error;
+    // } 
+
     return state;
  };
 
