0c29f89e506571e65f56a9d3d3c84498ed54c07f
mrmojica <mrmojica510@gmail.com>
2016-11-04 15:15:38 -0700

    created newproject component

---
+++ client/components/MainDisplay/NewProject.js
@@ -0,0 +1,80 @@
+var React = require('react');
+var actions = require('../../redux/actions/CardCategoriesActions');
+var connect = require('react-redux').connect;
+
+var empty = '';
+var NewProject = React.createClass({
+
+submitProject: function(event) {
+  event.preventDefault();
+if(!this.refs.projectTitle.value) {
+  alert('Please enter a project name.');
+
+} else {
+  //create new project action
+    console.log('project title ref', this.refs.projectTitle.value);
+    this.refs.projectTitle.value = '';
+}
+
+},
+
+onSelect: function() {
+   //run action to fetch selected dropdown project
+  console.log('this was selected', this.refs.selectedProject.value);
+},
+
+ render: function(){
+    var projectList = this.props.categories.map(function(title, index){
+      return (
+        //try and sort by project titles
+        <option key={index} value={title.title}>{empty + title.title}</option>
+      )
+    });
+
+   return (
+    <div className="Form wrapper">
+      <form>
+     
+        <input placeholder='Project Title...' ref="projectTitle" />
+        <span>
+          <button type='submit' onClick={this.submitProject}>Create Project</button>
+        </span>
+        <h5>Select Project:</h5>
+        <select ref="selectedProject" onselect={this.onSelect}>
+          {projectList}
+        </select>
+        <span>
+          <button type='submit' onClick={this.onSelect}>Go</button>
+        </span>
+      
+      </form>
+      
+    </div>
+ 
+   )
+ }
+
+ });
+
+
+
+
+
+
+
+var mapStateToProps = function(state, props) {
+	return {
+    //projectList: state.cardList.project || [],
+    categories: state.cardList.categories || [],
+    // categoryId: state
+    userId: state.cardList.userId
+	}
+};
+
+var Container = connect(mapStateToProps)(NewProject);
+
+module.exports = Container;
+
+
+
+
