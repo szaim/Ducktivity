45b9f3a5acd1f828b3ee05ca45db7bbd5ce476d6
szaim <zaimsofia81@gmail.com>
2016-11-09 16:34:11 -0500

    creating progress bar

---
+++ client/components/MainDisplay/ProgressBar.js
@@ -0,0 +1,21 @@
+var React = require("react");
+var Line = require("rc-progress").Line;
+
+var ProgressBar = React.createClass({
+	render: function() {
+		var containerStyle = {
+			width: '250px',
+		};
+		return (
+	        <div style={containerStyle}>
+	        	<Line 
+	        		percent={this.props.percent}  
+	        		strokeWidth="4" 
+	        		strokeColor={this.props.strokeColor} />
+	        </div>				
+
+		)
+	}
+})
+
+module.exports = ProgressBar;
--- client/components/TaskBoard/CategoryDisplay.js
+++ client/components/TaskBoard/CategoryDisplay.js
@@ -2,10 +2,11 @@ var React = require('react');
 var actions = require('../../redux/actions/CardCategoriesActions');
 var actionsOverview = require('../../redux/actions/overviewActions');
 var connect = require('react-redux').connect;
-import { DragDropContext } from 'react-dnd';
-import HTML5Backend from 'react-dnd-html5-backend';
 import { Draggable, Droppable } from 'react-drag-and-drop';
 var CardList = require('./CardList');
+var ProgressBar = require('./ProgressBar');
+
+ var percent;
 
 var CategoryDisplay = React.createClass({
 
@@ -28,11 +29,55 @@ var CategoryDisplay = React.createClass({
 
       console.log('delete cardId', TaskConstruct._id);
        console.log('delete original categoryId', card.category);
+     
+
+     
+          // this.props.categories[0].cards.filter((card)=> {
+          //   console.log("this.props.activeObjectiveIds", this.props.activeObjectiveIds)
+          //   this.props.activeObjectiveIds.indexOf(card.objective) != -1
+          //   console.log('card', card)
+          //   return (
+          //     blocked = card.length;
+          //     )
+          
+  
+          // })
+
+    var counter = 0;
+    var completed = 0;
+    for (var i = 0 ; i < this.props.categories.length; i++) {
+      var resultArr = this.props.categories[i].cards.filter((card)=>{
+          return this.props.activeObjectiveIds.indexOf(card.objective) != -1;
+        
+        })
+      console.log("resultArr.length", resultArr.length)
+      counter += resultArr.length
+    }
+
+    
+       var completedArr = this.props.categories[3].cards.filter((card)=>{
+          return this.props.activeObjectiveIds.indexOf(card.objective) != -1;
+        
+        })
+      console.log("completedArr.length", completedArr.length)
+      completed = completedArr.length
+      
+    console.log("counter", counter)
+     console.log("completed", completed)
+
+    if(completed === (counter - completed)) {
+        percent = 50;
+      }else if (counter === 0) {
+        percent = 100;
+      }
+      percent = Math.ceil(parseInt((completed / counter) * 100));
       this.props.dispatch(actions.postCard(TaskConstruct));
       this.props.dispatch(actions.deleteCard(TaskConstruct._id, card.category));
+     
     },
 
  render: function(){
+  console.log('percent', percent);
   if(this.props.categories){
     var displayCategories = this.props.categories.map((data, index)=> {
      return (
@@ -55,6 +100,9 @@ var CategoryDisplay = React.createClass({
 
    return (
      <div className='category-container'>
+      <div> 
+        <ProgressBar percent={percent} strokeColor='#FE8C6A'/>
+      </div>     
      <div className='task-categories'>
      {displayCategories}
      </div>
@@ -72,8 +120,10 @@ var CategoryDisplay = React.createClass({
 
 var mapStateToProps = function(state, props) {
 	return {
-    categories: state.cardList.categories || []
-
+    categories: state.cardList.categories || [],
+    activeObjectiveIds: state.overview.objectives.map(function(objective) {
+      return objective._id;
+    })
 	}
 };
 
